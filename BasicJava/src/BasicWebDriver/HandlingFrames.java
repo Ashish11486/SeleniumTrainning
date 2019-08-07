6ead4cc02f37363ee2fe85d616dbe515de58d356
ashis <ashis@DESKTOP-IBD602Q>
2019-08-07 17:13:21 -0400

    Added comment

--- BasicJava/src/BasicWebDriver/HandlingFrames.java
+++ BasicJava/src/BasicWebDriver/HandlingFrames.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class HandlingFrames {
-
+//webdriver
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
